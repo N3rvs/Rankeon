686e8a9c9c62ec4a46cad64edef6196e69dd265e
Guillermo Longueira Lopez <nervelessdev@gmail.com>
2025-07-04 17:13:16 +0000

    FirebaseAppError: The default Firebase app does not exist. Make sure you

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,10 @@
+// src/functions/index.ts
+import * as admin from 'firebase-admin';
+
+// Initialize Firebase Admin SDK. This must be done once, at the top.
+admin.initializeApp();
+
+// Now import the functions that use the initialized admin SDK.
 import { deleteChatHistory, sendMessageToFriend } from './chat';
 import { sendFriendRequest, respondToFriendRequest, removeFriend } from './friends';
 import {
