e9ea450f42e15bb031dbcbfd5b1f8f865cdf51ff
Guillermo Longueira Lopez <nervelessdev@gmail.com>
2025-10-23 23:37:15 +0000

    es decir que lo hagas como un prop {} entre llavces

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,7 +14,7 @@ export default function LoginPage() {
     <div className="w-full min-h-screen lg:grid lg:grid-cols-2">
       <div className="relative hidden flex-col items-center justify-center bg-gray-900 text-white lg:flex">
         <Image 
-            src="/login.png"
+            src={'/login.png'}
             alt="Gaming background"
             fill
             className="absolute inset-0 object-cover"
