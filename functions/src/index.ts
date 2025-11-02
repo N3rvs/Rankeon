28196ec53a700f4bafe071c390c3a4d1e4793ffe
Guillermo Longueira Lopez <nervelessdev@gmail.com>
2025-11-02 11:05:01 +0000

    sigue el error:Error: Cannot find module './api/admin/setGlobalRole' Req

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,129 +2,77 @@
 import "./admin";
 
 /* ========= ADMIN ========= */
-export { setGlobalRole }         from "./api/admin/setGlobalRole";
-export { grantFounder }          from "./api/admin/grantFounder";
-export { getManagedUsers }       from "./api/admin/getManagedUsers";
-export { updateUserPresence }    from "./api/admin/updateUserPresence";
-export { updateUserStatus }      from "./api/admin/updateUserStatus";
-export { adminUpdateUserStatus } from "./api/admin/adminUpdateUserStatus";
+export * from "./api/admin/setGlobalRole";
+export * from "./api/admin/grantFounder";
+export * from "./api/admin/getManagedUsers";
+export * from "./api/admin/updateUserPresence";
+export * from "./api/admin/updateUserStatus";
+export * from "./api/admin/adminUpdateUserStatus";
 
 /* ========= HONOR ========= */
-export { honorGive }        from "./api/honor/honorGive";
-export { honorRevoke }      from "./api/honor/honorRevoke";
-export { honorGetRankings } from "./api/honor/honorGetRankings";
-export { honorGetStats }    from "./api/honor/honorGetStats";
+export * from "./api/honor/honorGive";
+export * from "./api/honor/honorRevoke";
+export * from "./api/honor/honorGetRankings";
+export * from "./api/honor/honorGetStats";
 
 /* ========= NOTIFY / INBOX ========= */
-// Tus archivos de notify están en api/notify
-export { inboxAdd }      from "./api/notify/inboxAdd";
-export { inboxMarkRead } from "./api/notify/inboxMarkRead";
-export { inboxDelete }   from "./api/notify/inboxDelete";
-// Además tienes un listado de inbox en api/user:
-export { inboxList }     from "./api/user/inboxList";
+export * from "./api/notify/inboxAdd";
+export * from "./api/notify/inboxMarkRead";
+export * from "./api/notify/inboxDelete";
+export * from "./api/user/inboxList"; // This one is in user folder
 
 /* ========= MAINTENANCE ========= */
-export { nightlyCleanup }       from "./api/maintenance/cleanup";
-export { cleanupNotifications } from "./api/maintenance/cleanupNotifications";
-export { unbanExpiredUsers }    from "./api/maintenance/unbanExpiredUsers";
+export * from "./api/maintenance/cleanup";
+export * from "./api/maintenance/cleanupNotifications";
+export * from "./api/maintenance/unbanExpiredUsers";
 
 /* ========= ROOMS ========= */
-export {
-   roomsCreate,
-  roomsJoin,
-   roomsLeave,
-roomsSendMessage,
-} from "./api/rooms/rooms";
+export * from "./api/rooms/rooms";
 
 /* ========= SCRIMS ========= */
-export { scrimsChallenge } from "./api/scrims/challengeScrim";
+export * from "./api/scrims/challengeScrim";
 
 /* ========= SUPPORT / TICKETS ========= */
-export {
-  supportCreateTicket,
-  supportRespondTicket,
-   supportResolveTicket,
-} from "./api/support/tickets";
+export * from "./api/support/tickets";
 
 /* ========= TASKS ========= */
-// Tienes index.ts en /api/tasks con estos nombres:
-export {
-   taskCreate,
-   taskListMine,
-   taskUpdate,
-  taskDelete,
-  subtaskAdd,
-   subtaskToggle,
-} from "./api/tasks";
+export * from "./api/tasks";
 
 /* ========= TOURNAMENTS ========= */
-// Archivos según tus capturas; alias para nombres consistentes
-export {
-  tournamentsGenerateStructure,
-} from "./api/tournaments/generateStructure";
-
-export {
-  tournamentsReportBracketResult,
-} from "./api/tournaments/reportBracketResult";
-
-export {
-   tournamentsReportResult,
-} from "./api/tournaments/reportRoundRobinResult";
-
-export {
-  tournamentsRegisterTeam,
-} from "./api/tournaments/registerTeam";
-
-export {
-  tournamentsPropose,
-} from "./api/tournaments/proposeTournament";
-
-export {
- tournamentsReviewProposal,
-} from "./api/tournaments/reviewProposal";
-
-export { editTournament } from "./api/tournaments/editTournament";
-
-export {
-  tournamentsDelete,
-} from "./api/tournaments/deleteTournament";
+export * from "./api/tournaments/generateStructure";
+export * from "./api/tournaments/reportBracketResult";
+export * from "./api/tournaments/reportRoundRobinResult";
+export * from "./api/tournaments/registerTeam";
+export * from "./api/tournaments/proposeTournament";
+export * from "./api/tournaments/reviewProposal";
+export * from "./api/tournaments/editTournament";
+export * from "./api/tournaments/deleteTournament";
 
 /* ========= RANKINGS ========= */
-export {  clasificacionesGetTorneos }  from "./api/rankings/getTorneos";
-export {  clasificacionesGetScrims }   from "./api/rankings/getScrims";
-export { clasificacionesGetHonores }  from "./api/rankings/getHonores";
+export * from "./api/rankings/getTorneos";
+export * from "./api/rankings/getScrims";
+export * from "./api/rankings/getHonores";
 
 /* ========= USER (friends / teams / dm / presence / market) ========= */
-// DM
-export { dmGetOrCreate } from "./api/user/dmGetOrCreate";
-
-// Friends
-export { friendBlock }             from "./api/user/friendBlock";
-export { friendRemove }            from "./api/user/friendRemove";
-export { friendRequestRespond }    from "./api/user/friendRequestRespond";
-export { friendRequestSend }       from "./api/user/friendRequestSend";
-export { userListFriendsWithProfiles }       from "./api/user/getFriendProfiles";
-export { userListFriendRequests }  from "./api/user/userListFriendRequests";
-export { userListFriends }         from "./api/user/userListFriends";
-
-// Presence
-export { rtdbPresenceMirror } from "./api/user/rtdbPresenceMirror";
-
-// Teams: invites / join / members / roles
-export { teamGenerateInvite }       from "./api/user/teamGenerateInvite";
-export { teamInviteMember }         from "./api/user/teamInviteMember";
-export { teamInviteRespond }        from "./api/user/teamInviteRespond";
-export { teamJoinRequestCreate }    from "./api/user/teamJoinRequestCreate";
-export { teamJoinRequestRespond }   from "./api/user/teamJoinRequestRespond";
-export { teamKickMember }           from "./api/user/teamKickMember";
-export { teamSetMemberGameRoles }   from "./api/user/teamSetMemberGameRoles";
-export { teamSetMemberRole }        from "./api/user/teamSetMemberRole";
-
-// Teams: create / delete / join by code
-export { userCreateTeam }   from "./api/user/userCreateTeam";
-export { userDeleteTeam }   from "./api/user/userDeleteTeam";
-export { userJoinTeamByCode } from "./api/user/userJoinTeamByCode";
-
-// Market
-export { userListPlayers } from "./api/user/userListPlayers";
-export { userListTeams }   from "./api/user/userListTeams";
+export * from "./api/user/dmGetOrCreate";
+export * from "./api/user/friendBlock";
+export * from "./api/user/friendRemove";
+export * from "./api/user/friendRequestRespond";
+export * from "./api/user/friendRequestSend";
+export * from "./api/user/getFriendProfiles";
+export * from "./api/user/userListFriendRequests";
+export * from "./api/user/userListFriends";
+export * from "./api/user/rtdbPresenceMirror";
+export * from "./api/user/teamGenerateInvite";
+export * from "./api/user/teamInviteMember";
+export * from "./api/user/teamInviteRespond";
+export * from "./api/user/teamJoinRequestCreate";
+export * from "./api/user/teamJoinRequestRespond";
+export * from "./api/user/teamKickMember";
+export * from "./api/user/teamSetMemberGameRoles";
+export * from "./api/user/teamSetMemberRole";
+export * from "./api/user/userCreateTeam";
+export * from "./api/user/userDeleteTeam";
+export * from "./api/user/userJoinTeamByCode";
+export * from "./api/user/userListPlayers";
+export * from "./api/user/userListTeams";
